93142d4303e239874db6a5472a280915af3089a3
Wojciech <pietrzyk1977@gmail.com>
2017-12-19 19:16:06 +0100

    Plots commited

--- MyPlots/plot2.R
+++ MyPlots/plot2.R
@@ -1,3 +1,4 @@
+library(chron)
 
 #download unzip data
 fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
@@ -5,20 +6,21 @@ download.file(fileURL, destfile = "./data.zip")
 unzip("data.zip")
 
 #read data into R
-data <- read.table("./household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactor = FALSE, na.strings = "?")
+data <- read.table("./household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE, na.strings = "?")
+
 
 #subset data 
-dataFeb <- household[household$Date == "1/2/2007" | household$Date == "2/2/2007",]
+dataFeb <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
+dataFeb$Time <- chron(times = dataFeb$Time)
 
-#convert variables into Date/Time classes
-dataFeb$Date <- as.Date(dataFeb$Date, format = "%d/%m/%Y")
+#convert into POSIXlt
 dataFeb$Time <- paste(dataFeb$Date, dataFeb$Time)
-dataFeb$Time <- strptime(houseFeb$Time, format = "%d/%m/%Y %H:%M:%S")
+dataFeb$Time <- strptime(dataFeb$Time, format = "%d/%m/%Y %H:%M:%S")
 Sys.setlocale("LC_TIME", "English")
 
 #plot on a screen
 par(mfcol = c(1,1))
-plot(houseFeb$Time, houseFeb$Global_active_power, type = "l", xlab = " ", ylab = "Global Active Power (kilowatts)")
+plot(dataFeb$Time, dataFeb$Global_active_power, type = "l", xlab = " ", ylab = "Global Active Power (kilowatts)")
 
 #copy the plot into png file
 dev.copy(png, file = "plot2.png")
--- MyPlots/plot3.R
+++ MyPlots/plot3.R
@@ -8,19 +8,18 @@ unzip("data.zip")
 data <- read.table("./household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactor = FALSE, na.strings = "?")
 
 #subset data 
-dataFeb <- household[household$Date == "1/2/2007" | household$Date == "2/2/2007",]
+dataFeb <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
 
 #convert variables into Date/Time classes
-dataFeb$Date <- as.Date(dataFeb$Date, format = "%d/%m/%Y")
 dataFeb$Time <- paste(dataFeb$Date, dataFeb$Time)
-dataFeb$Time <- strptime(houseFeb$Time, format = "%d/%m/%Y %H:%M:%S")
+dataFeb$Time <- strptime(dataFeb$Time, format = "%d/%m/%Y %H:%M:%S")
 Sys.setlocale("LC_TIME", "English")
 
 #plot on a screen
 par(mfcol = c(1,1))
-with(houseFeb, plot(Time, Sub_metering_1, type = "l", col = "black", xlab = " ", ylab = "Energy sub metering"))
-points(houseFeb$Time, houseFeb$Sub_metering_2, type = "l", col = "red")
-points(houseFeb$Time, houseFeb$Sub_metering_3, type = "l", col = "blue")
+with(dataFeb, plot(Time, Sub_metering_1, type = "l", col = "black", xlab = " ", ylab = "Energy sub metering"))
+points(dataFeb$Time, dataFeb$Sub_metering_2, type = "l", col = "red")
+points(dataFeb$Time, dataFeb$Sub_metering_3, type = "l", col = "blue")
 legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 #copy the plot into png file
